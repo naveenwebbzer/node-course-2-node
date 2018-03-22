bd8c902dd03fef4d7107b91f80c288c32d2e8d56
naveen <naveenk@sourcesoftsolutions.com>
2018-03-22 20:23:55 +0530

    us server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,10 @@ var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 //var = '192.168.1.63:27017';
 //mongodb://username:password@host:port/database
-mongoose.connect(`mongodb://admin:admin123@192.168.1.63:27017/admin`);
+var paee='It_d@121!)(';
+mongoose.connect('mongodb://96.57.152.179:27017/admin',
+                 {user: 'admin', pass: 'It_d@121!)('});
+//mongoose.connect(`mongodb://admin:${paee}@96.57.152.179:27017/admin`);
+//mongoose.connect(`mongodb://admin:admin123@192.168.1.63:27017/admin`);
 //mongoose.connect(`mongodb://localhost:3000/mytest`);
 module.exports={mongoose};
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var app = express();
-var port=process.env.PORT || 4000;
+var port=process.env.PORT || 27017;
 app.use(bodyParser.json());
 
 app.post('/todo', (req, res) => {
