30880401bc4517cb3a696dc615d54b7ad67b6fd6
naveen <naveenk@sourcesoftsolutions.com>
2018-03-22 16:58:45 +0530

    test commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,7 @@
 var mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:3000/mytest');
+//var = '192.168.1.63:27017';
+//mongodb://username:password@host:port/database
+mongoose.connect(`mongodb://admin:admin123@192.168.1.63:27017/admin`);
+//mongoose.connect(`mongodb://localhost:3000/mytest`);
 module.exports={mongoose};
