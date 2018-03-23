35ec9334621df9887b55ac10ab62d5e18f01082c
naveen <naveenk@sourcesoftsolutions.com>
2018-03-23 17:12:48 +0530

    thisrd commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,9 @@ mongoose.Promise=global.Promise;
 //var = '192.168.1.63:27017';
 //mongodb://username:password@host:port/database
 var paee='It_d@121!)(';
-//mongoose.connect('mongodb://96.57.152.179:27017/admin',
-                // {user: 'admin', pass: 'It_d@121!)(',roles:[{role:"userAdminAnyDatabase"}]});
+mongoose.connect('mongodb://96.57.152.179:27017/admin',
+                 {user: 'admin', pass: 'It_d@121!)(',roles:[{role:"userAdminAnyDatabase"}]});
 //mongoose.connect(`mongodb://admin:${paee}@96.57.152.179:27017/admin`);
-mongoose.connect(`mongodb://admin:admin123@192.168.1.63:27017/admin`);
+//mongoose.connect(`mongodb://admin:admin123@192.168.1.63:27017/admin`);
 //mongoose.connect(`mongodb://localhost:3000/mytest`);
 module.exports={mongoose};
